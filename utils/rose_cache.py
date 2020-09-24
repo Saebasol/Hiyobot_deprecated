e807f094972398f44f2cb664f9ec2cb065f88aa4
SaidBySolo <saidbysolo@gmail.com>
2020-09-24 16:29:58 +0900

    feat(utils): all parsed

--- utils/rose_cache.py
+++ utils/rose_cache.py
@@ -7,16 +7,24 @@ def parse_value_url(value_url_list: list):
     if not value_url_list:
         return ["없음"]
     else:
-        return [f"[{value_url_dict['value']}](https://hitomi.la{value_url_dict.url})" for value_url_dict in value_url_list]
+        if not isinstance(value_url_list, list):
+            value_url_list = [value_url_list]
+        return [
+            f"[{value_url_dict['value']}](https://hitomi.la{value_url_dict.url})"
+            for value_url_dict in value_url_list
+        ]
 
-def make_embed_with_info(info:dict):
+
+def make_embed_with_info(info: dict):
     embed = discord.Embed(
         title=info.title["value"],
-        description=f"[{info.language['value']}]({info.language['url']})",
+        description="".join(parse_value_url(info.language)),
         url=info.title["url"],
     )
-    embed.add_field(name="번호", value=info.galleryid)
-    embed.add_field(name="타입", value=f"[{info.type['value']}]({info.type['url']})")
+    embed.add_field(
+        name="번호", value=f"[{info.galleryid}](https://hitomi.la/{info.galleryid}.html)"
+    )
+    embed.add_field(name="타입", value="".join(parse_value_url(info.type)))
     embed.add_field(name="직가", value=",".join(parse_value_url(info.artist)))
     embed.add_field(name="그룹", value=",".join(parse_value_url(info.group)))
     embed.add_field(name="원작", value=",".join(parse_value_url(info.series)))
