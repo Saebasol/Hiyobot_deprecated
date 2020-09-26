4f66d7daf264f4216c155ddfec06e7ccbd28fb8c
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-26 21:37:16 +0900

    feat(utils): add cache embed list

--- utils/rose_ext.py
+++ utils/rose_ext.py
@@ -7,8 +7,6 @@ def parse_value_url(value_url_list: list):
     if not value_url_list:
         return ["없음"]
     else:
-        if not isinstance(value_url_list, list):
-            value_url_list = [value_url_list]
         return [
             f"[{value_url_dict['value']}](https://hitomi.la{value_url_dict.url})"
             for value_url_dict in value_url_list
@@ -18,14 +16,13 @@ def parse_value_url(value_url_list: list):
 def make_embed_with_info(info: dict):
     embed = discord.Embed(
         title=info.title["value"],
-        description="".join(parse_value_url(info.language)),
+        description=f"[{info.language['value']}]({info.language['url']})",
         url=info.title["url"],
     )
-    embed.add_field(
-        name="번호", value=f"[{info.galleryid}](https://hitomi.la/{info.galleryid}.html)"
-    )
-    embed.add_field(name="타입", value="".join(parse_value_url(info.type)))
-    embed.add_field(name="직가", value=",".join(parse_value_url(info.artist)))
+    embed.set_thumbnail(url=f"https://doujinshiman.ga/proxy/{info.thumbnail}")
+    embed.add_field(name="번호", value=info.galleryid)
+    embed.add_field(name="타입", value=f"[{info.type['value']}]({info.type['url']})")
+    embed.add_field(name="작가", value=",".join(parse_value_url(info.artist)))
     embed.add_field(name="그룹", value=",".join(parse_value_url(info.group)))
     embed.add_field(name="원작", value=",".join(parse_value_url(info.series)))
     embed.add_field(name="캐릭터", value=",".join(parse_value_url(info.characters)))
@@ -33,11 +30,12 @@ def make_embed_with_info(info: dict):
     return embed
 
 
-class RoseCache(_Client):
+class RoseExt(_Client):
     def __init__(self, authorization):
         super().__init__(authorization)
         self.cache = aiocache.Cache()
 
-    async def cahe_list_embed(self, number):
-        lists = self.list_(number)
-        pass
+    async def cache_list_embed(self, number):
+        lists = await self.list_(number).list
+        embed = [make_embed_with_info(list_) for list_ in lists]
+        await self.cache.set("list_embed", embed)
