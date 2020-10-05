37f641db0f2b465ffb1a693e474556fa279a008c
Ryu JuHeon <saidbysolo@gmail.com>
2020-10-05 17:32:10 +0900

    refactor(cogs.events.ready): little optimize

--- Hiyobot/cogs/events/ready.py
+++ Hiyobot/cogs/events/ready.py
@@ -11,18 +11,12 @@ class Ready(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # login,status
     @commands.Cog.listener()
     async def on_ready(self):
-        # login
         print("Login.. : ")
         print(self.bot.user.name)
         print(self.bot.user.id)
-        print("======================")
-        print(f"{len(set(self.bot.get_all_members()))}명이 봇을 사용하고 있습니다..")
-        print("======================")
 
-        # Status
         game = discord.Game(f"&도움말 | {Hiyobot.__version__}")
         await self.bot.change_presence(status=discord.Status.online, activity=game)
 
