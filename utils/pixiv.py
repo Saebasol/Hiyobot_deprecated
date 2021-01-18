ec843240f4e6869f2dea156f9f28c058c997af8d
Sangoon_Is_Noob <40298015+sannoob@users.noreply.github.com>
2021-01-18 23:56:56 +0900

    refactor(recompile_date): refactor correct

--- utils/pixiv.py
+++ utils/pixiv.py
@@ -1,6 +1,7 @@
 import asyncio
 import re
 import time
+import datetime
 
 import aiocache
 import aiohttp
@@ -69,12 +70,7 @@ def shuffle_image_url(url: str):
 
 
 def recompile_date(date):
-    regex = re.compile(r"(\d{4})-(\d{2})-(\d{2})")
-    match = regex.search(date)
-    year = match.group(1)
-    month = match.group(2)
-    day = match.group(3)
-    return f"{year}년 {month}월 {day}일"
+    return datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%S%z").strftime("%Y년 %m월 %d일")
 
 async def request(method, endpoint, json=None):
     url = "https://www.pixiv.net/" + endpoint
