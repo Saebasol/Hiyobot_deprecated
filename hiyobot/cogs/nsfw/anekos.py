4303f68836e3f6b672ec67864b4f151948a54796
Ryu JuHeon <saidbysolo@gmail.com>
2021-05-18 22:46:52 +0900

    refactor!(anekos): using utils/anekos

--- hiyobot/cogs/nsfw/anekos.py
+++ hiyobot/cogs/nsfw/anekos.py
@@ -1,19 +1,26 @@
 import random
+from typing import Any
 
-import anekos
 import discord
-from discord.ext import commands
+
+from discord.ext.commands.cog import Cog
+from discord.ext.commands.context import Context
+from discord.ext.commands.core import command
 
 from Hiyobot.bot import Hiyobot
+from utils.anekos import get_url, sfw_tags, nsfw_tags
 
 
-class Nekos(commands.Cog):
+class Nekos(Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.neko_client = anekos.NekosLifeClient()
 
-    @commands.command(name="네코", aliases=["neko"])
-    async def _neko(self, ctx: commands.Context, tag: str = None):
+    @command(name="네코", aliases=["neko"])
+    async def _neko(
+        self,
+        ctx: Context,
+        tag: Any = None,
+    ):
         """
         귀여운 네코미미를 보여줍니다. 태그를 사용해서 검색도 가능합니다.
 
@@ -21,44 +28,31 @@ class Nekos(commands.Cog):
 
         사용 예시 : ``&네코`` 또는 ``&네코 wallpaper``
         """
-        embed = discord.Embed()
-
-        SFW_tags = [x.name for x in list(anekos.SFWImageTags)]
-        NSFW_tags = [x.name for x in list(anekos.NSFWImageTags)]
-
-        SFW_random = [anekos.SFWImageTags.NEKO, anekos.SFWImageTags.NEKOGIF]
-        NSFW_random = ["lewd", anekos.NSFWImageTags.NSFW_NEKO_GIF]
-
-        if tag == "도움말":
-            embed = discord.Embed(title="사용할 수 있는 태그 목록입니다.")
-            embed.add_field(name="전연령 태그", value="\n".join(SFW_tags))
-            if ctx.channel.nsfw:
-                embed.add_field(name="성인 태그", value="\n".join(NSFW_tags))
-            return await ctx.send(embed=embed)
-
+        embed = discord.Embed(colour=0xC44BAB)
+        embed.set_footer(
+            text="With Nekos.life",
+            icon_url="https://avatars.githubusercontent.com/u/34457007?s=200&v=4",
+        )
         if tag:
-            upper_tag = tag.upper()
-            if not ctx.channel.nsfw:
-                if upper_tag in NSFW_tags:
+            if tag in ["도움말", "help"]:
+                embed = discord.Embed(title="사용할 수 있는 태그 목록입니다.")
+                embed.add_field(name="전연령 태그", value="\n".join(sfw_tags))
+                if ctx.channel.is_nsfw():  # type:ignore
+                    embed.add_field(name="성인 태그", value="\n".join(nsfw_tags))
+                return await ctx.send(embed=embed)
+
+            if not ctx.channel.is_nsfw():  # type:ignore
+                if tag in nsfw_tags:
                     return await ctx.send(
                         "해당 태그는 성인 태그인 것 같습니다. 연령 제한이 설정된 채널에서 사용해주세요."
                     )
-                elif upper_tag in SFW_tags:
-                    image = await self.neko_client.image(upper_tag.lower())
-                else:
-                    return await ctx.send("해당 태그는 없어요! 태그는 ``&네코 도움말``을 통해 확인하실 수 있어요.")
-            else:
-                if upper_tag in NSFW_tags or upper_tag in SFW_tags:
-                    image = await self.neko_client.image(upper_tag.lower())
-                else:
-                    return await ctx.send("해당 태그는 없어요! 태그는 ``&네코 도움말``을 통해 확인하실 수 있어요.")
+            if tag not in sfw_tags or tag not in nsfw_tags:
+                return await ctx.send("해당 태그는 없어요! 태그는 ``&네코 도움말``을 통해 확인하실 수 있어요.")
         else:
-            image = await self.neko_client.image(
-                random.choice(SFW_random if not ctx.channel.nsfw else NSFW_random)
-            )
+            tag = random.choice(sfw_tags if not ctx.channel.is_nsfw() else nsfw_tags)  # type: ignore
 
-        embed.set_image(url=image.url)
-        await ctx.send(embed=embed)
+        img_url = await get_url(tag)
+        await ctx.send(embed=embed.set_image(url=img_url))
 
 
 def setup(bot: Hiyobot):
---
+++ utils/anekos.py
@@ -0,0 +1,90 @@
+from __future__ import annotations
+from typing import Literal, get_args
+
+from aiohttp import ClientSession
+
+BASE_URL = "https://nekos.life/api/"
+VERSION = "v2"
+
+URL = BASE_URL + VERSION
+
+# dummy ["v3","nekoapi_v3.1"]
+
+nsfw_tags_literal = Literal[
+    "Random_hentai_gif",
+    "cum_jpg",
+    "futanari",
+    "femdom",
+    "smallboobs",
+    "anal",
+    "nsfw_neko_gif",
+    "pussy_jpg",
+    "nsfw_avatar",
+    "boobs",
+    "blowjob",
+    "hentai",
+    "tits",
+    "erok",
+    "feetg",
+    "bj",
+    "erokemo",
+    "kuni",
+    "les",
+    "trap",
+    "hololewd",
+    "lewdk",
+    "solog",
+    "pussy",
+    "yuri",
+    "lewdkemo",
+    "lewd",
+    "pwankg",
+    "eron",
+    "keta",
+    "eroyuri",
+    "holoero",
+    "classic",
+    "feet",
+    "gasm",
+    "spank",
+    "erofeet",
+    "ero",
+    "solo",
+    "cum",
+]
+
+sfw_tags_literal = Literal[
+    "tickle",
+    "ngif",
+    "meow",
+    "poke",
+    "kiss",
+    "8ball",
+    "lizard",
+    "slap",
+    "cuddle",
+    "goose",
+    "avatar",
+    "fox_girl",
+    "hug",
+    "gecg",
+    "pat",
+    "smug",
+    "kemonomimi",
+    "holo",
+    "wallpaper",
+    "woof",
+    "baka",
+    "feed",
+    "neko",
+    "waifu",
+]
+
+sfw_tags = get_args(sfw_tags_literal)
+nsfw_tags = get_args(nsfw_tags_literal)
+
+
+async def get_url(tag: sfw_tags_literal | nsfw_tags_literal):
+    async with ClientSession() as cs:
+        async with cs.get(URL + tag) as r:
+            return (await r.json())["url"]
