215130ea1426b1e4581444e6003ffdfdad3c2a21
Ryu JuHeon <saidbysolo@gmail.com>
2021-05-19 10:31:17 +0900

    fix(bot): close #123 and some optimize

--- hiyobot/bot.py
+++ hiyobot/bot.py
@@ -1,63 +1,38 @@
-import logging
-import os
+from glob import glob
 
 import discord
-from discord.embeds import Embed
 from discord.ext.commands import Bot
-from discord.ext.commands.context import Context
-
-from utils.pixiv import PixivExt
-from utils.rose_ext import RoseExt
-
-logger = logging.getLogger("discord")
-logger.setLevel(logging.INFO)
-handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-handler.setFormatter(
-    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
-)
-logger.addHandler(handler)
 
 
 class Hiyobot(Bot):
-    def __init__(
-        self, command_prefix, help_command=None, description=None, **options
-    ) -> None:
+    def __init__(self, command_prefix, **options) -> None:
         super().__init__(
             command_prefix,
-            help_command=help_command,
-            description=description,
+            help_command=None,
+            description=None,
             **options,
         )
-        self.verify = os.environ.get("VERIFY")
-        self.rose = RoseExt(os.environ.get("heliotrope_auth"))
-        self.pixiv = PixivExt()
         self.heliotrope_issue = False
         self.maintenance = False
         self.maintenance_message = ""
 
 
 def load_cogs(bot: Hiyobot):
-    extensions = [
-        "jishaku",
-        "admin.issue",
-        "events.error",
-        "events.ready",
-        "general.help",
-        "general.patchnote",
-        "general.info",
-        "general.auth",
-        "nsfw.anekos",
-        "nsfw.heliotrope",
-        "nsfw.pixiv",
-    ]
-
+    extensions = list(
+        map(
+            lambda path: path.replace("./", "")
+            .replace(".py", "")
+            .replace("\\", ".")
+            .replace("/", "."),
+            filter(lambda path: "__" not in path, glob("./Hiyobot/cogs/*/*")),
+        )
+    )
+    extensions.append("jishaku")
     failed_list = []
 
     for extension in extensions:
         try:
-            bot.load_extension(
-                "Hiyobot.cogs." + extension if "." in extension else extension
-            )
+            bot.load_extension(extension)
         except Exception as e:
             print(e)
             failed_list.append(extension)
@@ -65,5 +40,8 @@ def load_cogs(bot: Hiyobot):
     return failed_list
 
 
-intents = discord.Intents.default()
-bot = Hiyobot(command_prefix="&", intents=intents)
+def run(token: str):
+    intents = discord.Intents.default()
+    bot = Hiyobot(command_prefix="&", intents=intents)
+    load_cogs(bot)
+    bot.run(token)
