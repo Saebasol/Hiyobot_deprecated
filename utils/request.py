bed5dcbaa1a9170406f76d890b60d5f838285b88
Ryu JuHeon <saidbysolo@gmail.com>
2021-05-22 21:37:40 +0900

    feat(reqeust): add request class

---
+++ utils/request.py
@@ -0,0 +1,37 @@
+from typing import Any, Literal, Optional
+from aiohttp import ClientSession
+from dataclasses import dataclass
+
+
+@dataclass
+class Response:
+    status: int
+    body: Any
+
+
+class Request:
+    def __init__(self) -> None:
+        self.session: Optional[ClientSession] = None
+
+    async def request(
+        self,
+        method: Literal["GET", "POST"],
+        url: str,
+        return_method: Literal["json", "text", "read"],
+        **kwargs: Any
+    ):
+        if not self.session:
+            self.session = ClientSession()
+
+        response = await self.session.request(method, url, **kwargs)
+        return Response(response.status, await getattr(response, return_method)())
+
+    async def get(
+        self, url: str, return_method: Literal["json", "text", "read"], **kwargs: Any
+    ):
+        return await self.request("GET", url, return_method, **kwargs)
+
+    async def post(
+        self, url: str, return_method: Literal["json", "text", "read"], **kwargs: Any
+    ):
+        return await self.request("POST", url, return_method, **kwargs)
