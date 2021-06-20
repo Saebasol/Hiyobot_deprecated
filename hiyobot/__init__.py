bf453416bd2ac2e1d129af57c4eedb293a6dffdb
ombe <h3236516@gmail.com>
2021-06-20 21:47:35 +0900

    style(Hiyobot): bump up 4.0.0-alpha.1 to 4.0.0-alpha.2

--- hiyobot/__init__.py
+++ hiyobot/__init__.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
-__version__ = "4.0.0-alpha.1"
+__version__ = "4.0.0-alpha.2"
 
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 
