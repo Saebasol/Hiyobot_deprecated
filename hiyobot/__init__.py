bedcad87ab1a91864bb25a29978d8b031aac9079
Ryu JuHeon <saidbysolo@gmail.com>
2021-06-21 16:55:34 +0900

    style(hiyobot): Bump up 4.0.0-alpha.3 to 4.0.0-alpha.5

--- hiyobot/__init__.py
+++ hiyobot/__init__.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
-__version__ = "4.0.0-alpha.3"
+__version__ = "4.0.0-alpha.5"
 
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 
-version_info = VersionInfo(major=4, minor=0, micro=0, releaselevel="alpha", serial=3)
+version_info = VersionInfo(major=4, minor=0, micro=0, releaselevel="alpha", serial=5)
