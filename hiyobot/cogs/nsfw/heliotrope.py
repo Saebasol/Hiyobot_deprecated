d028f0e555fcdb7962a430cdf1f071ab54533b73
green1052 <green1052@protonmail.ch>
2021-07-27 22:16:48 +0900

    fix(heliotrope): close viewer was edit to not found embed (#169)
    * fix: 정보를 찾지 못했습니다.
    
    * Update heliotrope.py

--- hiyobot/cogs/nsfw/heliotrope.py
+++ hiyobot/cogs/nsfw/heliotrope.py
@@ -25,9 +25,7 @@ class Heliotrope(commands.Cog):
     async def _info(self, ctx: commands.Context, index: int):
         """
         작품 번호를 입력하면 히토미에서 해당 작품 정보를 가져옵니다.
-
         사용할 수 있는 값 : 작품 번호(필수)
-
         사용 예시 : ``&번호 1496588``
         """
         msg = await ctx.send(embed=discord.Embed(title="정보를 요청합니다. 잠시만 기다려주세요."))
@@ -55,9 +53,7 @@ class Heliotrope(commands.Cog):
     async def _list(self, ctx: commands.Context, num: int = 1):
         """
         히토미에서 최근 올라온 한국어 작품을 가져옵니다.
-
         사용할 수 있는 값 : 페이지(입력하지 않을 경우 1)
-
         사용 예시 : ``&리스트`` 또는 ``&리스트 2``
         """
         msg = await ctx.send(embed=discord.Embed(title="정보를 요청합니다. 잠시만 기다려주세요"))
@@ -72,14 +68,12 @@ class Heliotrope(commands.Cog):
     async def _viewer(self, ctx: commands.Context, index: int):
         """
         작품 번호를 입력하면 디스코드 내에서 보여줍니다.
-
         인자값: 작품 번호(필수)
-
         사용법: ``&뷰어 1496588``
         """
         msg = await ctx.send(embed=discord.Embed(title="정보를 요청합니다. 잠시만 기다려주세요."))
         if embed := await self.bot.mintchoco.viewer_embed(index):
-            await pagenator(self.bot, ctx, msg, embed)
+            return await pagenator(self.bot, ctx, msg, embed)
 
         await msg.edit(embed=discord.Embed(title="정보를 찾지 못했습니다."))
 
